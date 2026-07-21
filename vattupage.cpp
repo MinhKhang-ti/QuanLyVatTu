7aa7e66bfbf6c11bdcd026de2e23ac4613675314
MinhKhang-ti <minhkhang231177@gmail.com>
2026-07-21 23:30:07 +0700

    feature vat tu

--- vattupage.cpp
+++ vattupage.cpp
@@ -1,29 +1,27 @@
 #include "vattupage.h"
+#include "fileio.h"
 #include "ui_vattupage.h"
 #include "vattulogic.h"
-
 #include <QIntValidator>
 #include <QRegularExpressionValidator>
 #include <QHeaderView>
+#include <QSignalBlocker>
 
-VatTuPage::VatTuPage(TreeVT &rootRef, QWidget *parent) 
-    : QWidget(parent), ui(new Ui::VatTuPage), root(rootRef) 
+VatTuPage::VatTuPage(TreeVT &rootRef, QWidget *parent)
+    : QWidget(parent), ui(new Ui::VatTuPage), root(rootRef)
 {
     ui->setupUi(this);
-
     // Cấu hình validator
     ui->maVTEdit->setValidator(new QRegularExpressionValidator(
         QRegularExpression("[A-Za-z0-9]{0,10}"), this));
     ui->soLuongEdit->setValidator(new QIntValidator(0, 999999, this));
-
     // Cấu hình bảng hiển thị
     ui->table->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
     ui->table->setEditTriggers(QAbstractItemView::NoEditTriggers);
     ui->table->setSelectionBehavior(QAbstractItemView::SelectRows);
-
     ui->errorLabel->setVisible(false);
     ui->themButton->setEnabled(false);
-
+    ui->suaButton->setEnabled(false);
     // Kết nối các tín hiệu
     connect(ui->maVTEdit, &QLineEdit::textChanged, this, &VatTuPage::validateForm);
     connect(ui->tenVTEdit, &QLineEdit::textChanged, this, &VatTuPage::validateForm);
@@ -31,7 +29,10 @@ VatTuPage::VatTuPage(TreeVT &rootRef, QWidget *parent)
     connect(ui->soLuongEdit, &QLineEdit::textChanged, this, &VatTuPage::validateForm);
     connect(ui->themButton, &QPushButton::clicked, this, &VatTuPage::onThemClicked);
     connect(ui->xoaButton, &QPushButton::clicked, this, &VatTuPage::onXoaClicked);
+    connect(ui->table, &QTableWidget::itemSelectionChanged, this, &VatTuPage::onTableSelectionChanged);
+    connect(ui->suaButton, &QPushButton::clicked, this, &VatTuPage::onSuaClicked);
 
+     docVatTu(root, FILE_VATTU);
     lamMoiBang();
 }
 
@@ -69,10 +70,8 @@ void VatTuPage::onThemClicked() {
         ui->errorLabel->setVisible(true);
         return;
     }
-    ui->maVTEdit->clear();
-    ui->tenVTEdit->clear();
-    ui->dvtEdit->clear();
-    ui->soLuongEdit->clear();
+    resetForm();
+    luuVatTu(root, FILE_VATTU);
     lamMoiBang();
 }
 
@@ -82,13 +81,52 @@ void VatTuPage::onXoaClicked() {
     QString mavt = ui->table->item(row, 0)->text();
     std::string loi;
     xoaVT(root, mavt.toStdString().c_str(), loi);
+    resetForm();
+    luuVatTu(root, FILE_VATTU);
+    lamMoiBang();
+}
+
+void VatTuPage::onTableSelectionChanged() {
+    int row = ui->table->currentRow();
+    if (row < 0) {
+        ui->suaButton->setEnabled(false);
+        return;
+    }
+    maVTDangSua = ui->table->item(row, 0)->text();
+    ui->maVTEdit->setText(maVTDangSua);
+    ui->tenVTEdit->setText(ui->table->item(row, 1)->text());
+    ui->dvtEdit->setText(ui->table->item(row, 2)->text());
+    ui->soLuongEdit->setText(ui->table->item(row, 3)->text());
+
+    ui->maVTEdit->setReadOnly(true);
+    ui->soLuongEdit->setReadOnly(true);
+    ui->themButton->setEnabled(false);
+    ui->suaButton->setEnabled(true);
+}
+
+void VatTuPage::onSuaClicked() {
+    if (maVTDangSua.isEmpty()) return;
+
+    std::string loi;
+    bool ok = suaVT(root,
+                    maVTDangSua.toStdString().c_str(),
+                    ui->tenVTEdit->text().toStdString().c_str(),
+                    ui->dvtEdit->text().toStdString().c_str(),
+                    loi);
+    if (!ok) {
+        ui->errorLabel->setText(QString::fromStdString(loi));
+        ui->errorLabel->setVisible(true);
+        return;
+    }
+
+    resetForm();
+    luuVatTu(root, FILE_VATTU);
     lamMoiBang();
 }
 
 void VatTuPage::lamMoiBang() {
     int soLuong = 0;
     VATTU* ds = duyetTheoTen(root, soLuong);
-
     ui->table->setRowCount(soLuong);
     for (int i = 0; i < soLuong; i++) {
         ui->table->setItem(i, 0, new QTableWidgetItem(ds[i].MAVT));
@@ -96,6 +134,27 @@ void VatTuPage::lamMoiBang() {
         ui->table->setItem(i, 2, new QTableWidgetItem(ds[i].DVT));
         ui->table->setItem(i, 3, new QTableWidgetItem(QString::number(ds[i].SoLuongTon)));
     }
-
     delete[] ds; // giai phong mang dong sau khi da dung xong
+}
+
+void VatTuPage::resetForm() {
+    // Chan tin hieu textChanged trong luc reset, tranh goi validateForm() oan
+    QSignalBlocker b1(ui->maVTEdit);
+    QSignalBlocker b2(ui->tenVTEdit);
+    QSignalBlocker b3(ui->dvtEdit);
+    QSignalBlocker b4(ui->soLuongEdit);
+
+    ui->maVTEdit->clear();
+    ui->tenVTEdit->clear();
+    ui->dvtEdit->clear();
+    ui->soLuongEdit->clear();
+    ui->maVTEdit->setReadOnly(false);
+    ui->soLuongEdit->setReadOnly(false);
+    ui->suaButton->setEnabled(false);
+    maVTDangSua.clear();
+
+    // QSignalBlocker tu dong "mo lai" khi ra khoi ham (het scope)
+    // nen goi validateForm() 1 lan duy nhat, thu cong, de cap nhat dung trang thai nut
+    ui->errorLabel->setVisible(false);
+    ui->themButton->setEnabled(false);
 }
--- vattupage.h
+++ vattupage.h
@@ -19,10 +19,14 @@ private slots:
     void validateForm();
     void onThemClicked();
     void onXoaClicked();
+    void onTableSelectionChanged();
+    void onSuaClicked();
+    void resetForm();
 
 private:
     Ui::VatTuPage *ui;
     TreeVT &root; // tham chieu toi cay dung chung, khong so huu rieng nua
+    QString maVTDangSua; // rong = dang o che do them moi, khac rong = dang sua dong nay
 
     void lamMoiBang();
 };
